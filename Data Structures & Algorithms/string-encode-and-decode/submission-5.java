227701ccd107ec6ebae01087a30da984a786dd59
Aaryan Nagrale <158316872+Aaryan10-11@users.noreply.github.com>
2026-09-26 13:32:25 +0530

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.java
@@ -0,0 +1,56 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        if(strs.isEmpty()) return "";
+
+        StringBuilder sb = new StringBuilder();
+        List<Integer> sizes = new ArrayList<>();
+
+        for(String str : strs ){
+            sizes.add(str.length());
+        }
+         for(int size : sizes){
+            sb.append(size).append(',');
+         }
+
+         sb.append('#');
+
+        for(String str :strs ){
+            sb.append(str);
+        }
+
+        return sb.toString();
+    }
+
+
+
+    public List<String> decode(String str) {
+
+        if(str.length()==0){
+            return new ArrayList<>();
+        }
+
+        List<String> res = new ArrayList();
+        List <Integer> sizes = new ArrayList<>();
+
+        int i=0;
+
+        while(str.charAt(i)!='#'){
+            StringBuilder sb= new StringBuilder();
+
+            while(str.charAt(i) != ','){
+                sb.append(str.charAt(i));
+                i++;
+            }
+            sizes.add(Integer.parseInt(sb.toString()));
+            i++;
+
+        }
+        i++;
+        for(int s : sizes){
+            res.add(str.substring(i,i+s));
+            i+=s;
+        } 
+        return res;
+    }
+}
